9441efb155ffa93bcc9651b5877d6387a02455cf
Maha Zayed <mzayed@Mahas-MacBook-Pro.local>
2019-01-05 16:21:43 -0500

    Added some files for initial setup of login/registration feature of app

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Testing 123
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                name: ''
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
